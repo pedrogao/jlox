a5caca6b139f7fa21cf8fc316123485aa5e6d622
pedrogao <1312342604@qq.com>
2022-02-28 12:48:44 +0800

    feat: init project

---
+++ README.md
@@ -0,0 +1,8 @@
+# jlox
+
+> lox implement by java
+
+
+
+## 参考资料
+- https://craftinginterpreters.com/contents.html
---
+++ src/main/java/com/pedrogao/jlox/Main.java
@@ -0,0 +1,7 @@
+package com.pedrogao.jlox;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("jlox");
+    }
+}
